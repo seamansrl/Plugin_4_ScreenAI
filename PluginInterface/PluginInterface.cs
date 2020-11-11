5b54632f1f7b3e81715b8a770e804ca570ade760
SeaMan SRL <g3ne51s@hotmail.com>
2020-11-11 13:40:23 -0300

    Add files via upload

---
+++ PluginInterface/PluginInterface.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace PluginInterface
+{
+	public interface IPlugin
+	{
+		IPluginHost Host {get;set;}
+		
+		string Name {get;}
+		string Description {get;}
+		string Author {get;}
+		string Version {get;}
+
+		String Input { set; }
+		String Output { get; }
+
+		System.Windows.Forms.UserControl MainInterface {get;}
+		
+		void Initialize();
+		void Dispose();
+	
+	}
+	
+	public interface IPluginHost
+	{
+		void Feedback(string Feedback, IPlugin Plugin);	
+	}
+}
